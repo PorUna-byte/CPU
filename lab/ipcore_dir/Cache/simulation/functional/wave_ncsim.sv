cc853326076e05f6962facff2321e219fa0f39ed
PorUna-byte <2596367262@qq.com>
2021-01-12 00:35:08 +0800

    cache

---
+++ lab/ipcore_dir/Cache/simulation/functional/wave_ncsim.sv
@@ -0,0 +1,47 @@
+
+
+ 
+ 
+ 
+
+ 
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+      waveform add -signals /Cache_tb/status
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_ACLK
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWADDR
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWVALID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWREADY
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWLEN
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWSIZE
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWBURST
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_AWID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_WDATA
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_WSTRB
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_WVALID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_WREADY
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_WLAST
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_BID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_BRESP
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_BVALID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_BREADY
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARADDR
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARLEN
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARSIZE
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARBURST
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARVALID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_ARREADY
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_RID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_RDATA
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_RRESP
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_RLAST
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_RVALID
+    waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_AXI_RREADY
+      waveform add -signals /Cache_tb/Cache_synth_inst/bmg_port/S_ARESETN
+
+console submit -using simulator -wait no "run"
---
+++ lab/readme.txt
@@ -0,0 +1,19 @@
+本文件夹为计算机组织与体系结构课程实验4-Cache实验的文件目录
+
+--readme.txt 本文档
+
+--lab.xise  实验所用的项目，直接双击即可打开完整的实验ISE工程
+
+--axi_ram.coe 自动化测试所使用的coe文件，用于初始化存储器
+
+--cache.v 实验所使用cache的verilog源码文件
+
+--cache_data.v 实验所使用cache的数据存储器的verilog源码文件
+
+--cache_tagv.v 实验所使用cache的目录表的verilog源码文件
+
+--cache_tb.v 自动化测试环境的源码文件，使用coremark来作为测试用例对cache进行测试
+
+--trace 存放trace文件的文件夹
+   --cache_addr_trace.txt 指令访存的访存地址序列
+   --cache_data_trace.txt 指令访存的访存数据序列
